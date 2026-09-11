053a4b13ad0e7545896d462fcd40647b0fae6a01
Raivo Hundt <canman.systems@gmail.com>
2026-09-11 19:39:48 +0300

    Remove broken products folder

--- src/data/products.ts
+++
@@ -1,6 +0,0 @@
-﻿import { arvutid } from './catalog/arvutid';
-import { komponendid } from './catalog/komponendid';
-import { printerid } from './catalog/printerid';
-
-export const allProducts = [...arvutid, ...komponendid, ...printerid];
-export const products = allProducts;
--- src/data/products/index.ts
+++
@@ -1,6 +0,0 @@
-﻿import { arvutid } from './arvutid';
-import { komponendid } from './komponendid';
-import { printerid } from './printerid';
-
-export const allProducts = [...arvutid, ...komponendid, ...printerid];
-export const products = allProducts;
