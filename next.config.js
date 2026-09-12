b03e67f3075fc4d2af551db6a38ee9f10d130ffc
raivoto <raivoto@gmail.com>
2026-09-12 21:19:11 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   basePath: '/canman.github.io',
+  assetPrefix: '/canman.github.io/',
   images: { unoptimized: true },
+  trailingSlash: true,
 }
 module.exports = nextConfig
