6467449e5823ed323efa59797e512c457c58fcfe
raivoto <raivoto@gmail.com>
2026-09-12 21:22:12 +0300

    Update index.ts

--- src/data/productCatalog/index.ts
+++ src/data/productCatalog/index.ts
@@ -1,15 +1,8 @@
 import { arvutid } from './arvutid';
 import { komponendid } from './komponendid';
 import { printerid } from './printerid';
+import { autoProducts } from './autoProducts';
 
-// proovime laadida autoProducts kui olemas
-let autoProducts: any[] = [];
-try {
- // @ts-ignore
-  const mod = require('./autoProducts');
-  autoProducts = mod.autoProducts || [];
-} catch {}
-
-export const allProducts: any[] = [...arvutid,...komponendid,...printerid,...autoProducts];
+export const allProducts: any[] = [...arvutid, ...komponendid, ...printerid, ...autoProducts];
 export const products: any[] = allProducts;
 export const PRODUCTS: any[] = allProducts;
