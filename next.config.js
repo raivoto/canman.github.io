1ebe9f52999e6419e1b359fd09003d1548e94df7
raivoto <raivoto@gmail.com>
2026-09-12 19:54:01 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/canman.github.io',
+  images: { unoptimized: true },
+}
+module.exports = nextConfig
