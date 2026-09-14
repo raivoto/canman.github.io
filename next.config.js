6a547519b20dce565d8465743430fab59ad43144
raivoto <raivoto@gmail.com>
2026-09-14 20:45:51 +0300

    Remove unused Next.js config options

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // output: 'export' - kustutatud, Vercel ei vaja
 }
-
-module.exports = nextConfig
