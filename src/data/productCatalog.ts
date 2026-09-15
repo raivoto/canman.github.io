408fbd5452408371ca11bfcaf1bdcbf33a6001b8
Raivo <raivo@canman.ee>
2026-09-15 15:17:58 +0300

    kustuta vana import-xls workflow

--- src/data/productCatalog.ts
+++ src/data/productCatalog.ts
@@ -1,27 +1,44 @@
-// AUTO - ära muuda käsitsi, genereeritakse XLS-ist
-import cpu from './products/lauarvuti-osad-kasut/protsessor-pc.json';
-import combo from './products/lauarvuti-osad-kasut/combo.json';
-import emaplaat from './products/lauarvuti-osad-kasut/emaplaat.json';
-import ram from './products/lauarvuti-osad-kasut/m-lu-dimm.json';
-import monitorid from './products/monitorid/monitorid-22-25.json';
-import laserprinter from './products/printerid-toonerid/laserprinter.json';
-import toonerLaser from './products/printerid-toonerid/tooner-laserprindile.json';
-import toonerid from './products/printerid-toonerid/toonerid-tindid.json';
-import used3 from './products/desktop-kasutatud/used-3-gen.json';
-import used2 from './products/desktop-kasutatud/used-2-gen.json';
-import used4 from './products/desktop-kasutatud/used-4-gen.json';
-import used7 from './products/desktop-kasutatud/used-7-gen.json';
-import used6 from './products/desktop-kasutatud/used-6-gen.json';
-import laptop14 from './products/laptops/laptop-14.json';
-import laptop14a from './products/laptops-used/laptop-14.json';
-import laptop1213 from './products/laptops-used/laptop-12-13-used.json';
-import laptop15 from './products/laptops-used/laptop-15-used.json';
-import laptop14used from './products/laptops-used/laptop-14-used.json';
+// AUTO 42
+import p_lauarvuti_osad_kasut_protsessor_pc from './products/lauarvuti-osad-kasut/protsessor-pc.json';
+import p_lauarvuti_osad_kasut_combo from './products/lauarvuti-osad-kasut/combo.json';
+import p_lauarvuti_osad_kasut_emaplaat from './products/lauarvuti-osad-kasut/emaplaat.json';
+import p_lauarvuti_osad_kasut_m_lu_dimm_pc3 from './products/lauarvuti-osad-kasut/m-lu-dimm/pc3.json';
+import p_lauarvuti_osad_kasut_m_lu_dimm_pc4 from './products/lauarvuti-osad-kasut/m-lu-dimm/pc4.json';
+import p_lauarvuti_osad_kasut_m_lu_dimm_pc2 from './products/lauarvuti-osad-kasut/m-lu-dimm/pc2.json';
+import p_monitorid_monitorid_22_25 from './products/monitorid/monitorid-22-25.json';
+import p_printerid_toonerid_laserprinter from './products/printerid-toonerid/laserprinter.json';
+import p_printerid_toonerid_tooner_laserprindile from './products/printerid-toonerid/tooner-laserprindile.json';
+import p_printerid_toonerid_toonerid_tindid from './products/printerid-toonerid/toonerid-tindid.json';
+import p_desktop_kasutatud_used_3_gen from './products/desktop-kasutatud/used-3-gen.json';
+import p_desktop_kasutatud_used_2_gen from './products/desktop-kasutatud/used-2-gen.json';
+import p_desktop_kasutatud_used_4_gen from './products/desktop-kasutatud/used-4-gen.json';
+import p_desktop_kasutatud_used_7_gen from './products/desktop-kasutatud/used-7-gen.json';
+import p_desktop_kasutatud_used_6_gen from './products/desktop-kasutatud/used-6-gen.json';
+import p_laptops_used_laptop_14 from './products/laptops-used/laptop-14.json';
+import p_laptops_used_laptop_12_13_used from './products/laptops-used/laptop-12-13-used.json';
+import p_laptops_used_laptop_15_used from './products/laptops-used/laptop-15-used.json';
+import p_laptops_used_laptop_14_used from './products/laptops-used/laptop-14-used.json';
+import p_laptops_laptop_14 from './products/laptops/laptop-14.json';
 
 export const allProducts = [
-  ...cpu, ...combo, ...emaplaat, ...ram,
-  ...monitorid,
-  ...laserprinter, ...toonerLaser, ...toonerid,
-  ...used3, ...used2, ...used4, ...used7, ...used6,
-  ...laptop14, ...laptop14a, ...laptop1213, ...laptop15, ...laptop14used
+ ...p_lauarvuti_osad_kasut_protsessor_pc,
+ ...p_lauarvuti_osad_kasut_combo,
+ ...p_lauarvuti_osad_kasut_emaplaat,
+ ...p_lauarvuti_osad_kasut_m_lu_dimm_pc3,
+ ...p_lauarvuti_osad_kasut_m_lu_dimm_pc4,
+ ...p_lauarvuti_osad_kasut_m_lu_dimm_pc2,
+ ...p_monitorid_monitorid_22_25,
+ ...p_printerid_toonerid_laserprinter,
+ ...p_printerid_toonerid_tooner_laserprindile,
+ ...p_printerid_toonerid_toonerid_tindid,
+ ...p_desktop_kasutatud_used_3_gen,
+ ...p_desktop_kasutatud_used_2_gen,
+ ...p_desktop_kasutatud_used_4_gen,
+ ...p_desktop_kasutatud_used_7_gen,
+ ...p_desktop_kasutatud_used_6_gen,
+ ...p_laptops_used_laptop_14,
+ ...p_laptops_used_laptop_12_13_used,
+ ...p_laptops_used_laptop_15_used,
+ ...p_laptops_used_laptop_14_used,
+ ...p_laptops_laptop_14
 ] as any[];
