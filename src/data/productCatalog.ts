c8568cf31a3aab4ae70a8cc1687212610ddc3878
Raivo <raivo@canman.ee>
2026-09-15 16:46:19 +0300

    import all images from xls

--- src/data/productCatalog.ts
+++ src/data/productCatalog.ts
@@ -1,44 +1,181 @@
-// AUTO 42
-import p_lauarvuti_osad_kasut_protsessor_pc from './products/lauarvuti-osad-kasut/protsessor-pc.json';
-import p_lauarvuti_osad_kasut_combo from './products/lauarvuti-osad-kasut/combo.json';
-import p_lauarvuti_osad_kasut_emaplaat from './products/lauarvuti-osad-kasut/emaplaat.json';
-import p_lauarvuti_osad_kasut_m_lu_dimm_pc3 from './products/lauarvuti-osad-kasut/m-lu-dimm/pc3.json';
-import p_lauarvuti_osad_kasut_m_lu_dimm_pc4 from './products/lauarvuti-osad-kasut/m-lu-dimm/pc4.json';
-import p_lauarvuti_osad_kasut_m_lu_dimm_pc2 from './products/lauarvuti-osad-kasut/m-lu-dimm/pc2.json';
-import p_monitorid_monitorid_22_25 from './products/monitorid/monitorid-22-25.json';
-import p_printerid_toonerid_laserprinter from './products/printerid-toonerid/laserprinter.json';
-import p_printerid_toonerid_tooner_laserprindile from './products/printerid-toonerid/tooner-laserprindile.json';
-import p_printerid_toonerid_toonerid_tindid from './products/printerid-toonerid/toonerid-tindid.json';
-import p_desktop_kasutatud_used_3_gen from './products/desktop-kasutatud/used-3-gen.json';
-import p_desktop_kasutatud_used_2_gen from './products/desktop-kasutatud/used-2-gen.json';
-import p_desktop_kasutatud_used_4_gen from './products/desktop-kasutatud/used-4-gen.json';
-import p_desktop_kasutatud_used_7_gen from './products/desktop-kasutatud/used-7-gen.json';
-import p_desktop_kasutatud_used_6_gen from './products/desktop-kasutatud/used-6-gen.json';
-import p_laptops_used_laptop_14 from './products/laptops-used/laptop-14.json';
-import p_laptops_used_laptop_12_13_used from './products/laptops-used/laptop-12-13-used.json';
-import p_laptops_used_laptop_15_used from './products/laptops-used/laptop-15-used.json';
-import p_laptops_used_laptop_14_used from './products/laptops-used/laptop-14-used.json';
-import p_laptops_laptop_14 from './products/laptops/laptop-14.json';
+// Auto-generated from data\import\Arvutiosad.xls on 2026-09-15T13:46:01.916Z
+// Tooteid: 17
 
 export const allProducts = [
- ...p_lauarvuti_osad_kasut_protsessor_pc,
- ...p_lauarvuti_osad_kasut_combo,
- ...p_lauarvuti_osad_kasut_emaplaat,
- ...p_lauarvuti_osad_kasut_m_lu_dimm_pc3,
- ...p_lauarvuti_osad_kasut_m_lu_dimm_pc4,
- ...p_lauarvuti_osad_kasut_m_lu_dimm_pc2,
- ...p_monitorid_monitorid_22_25,
- ...p_printerid_toonerid_laserprinter,
- ...p_printerid_toonerid_tooner_laserprindile,
- ...p_printerid_toonerid_toonerid_tindid,
- ...p_desktop_kasutatud_used_3_gen,
- ...p_desktop_kasutatud_used_2_gen,
- ...p_desktop_kasutatud_used_4_gen,
- ...p_desktop_kasutatud_used_7_gen,
- ...p_desktop_kasutatud_used_6_gen,
- ...p_laptops_used_laptop_14,
- ...p_laptops_used_laptop_12_13_used,
- ...p_laptops_used_laptop_15_used,
- ...p_laptops_used_laptop_14_used,
- ...p_laptops_laptop_14
-] as any[];
+  {
+    "id": "prod-0",
+    "title": "Protsessor Intel i5-4440   ",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "I-protsessor_i5_desktop.png"
+    ]
+  },
+  {
+    "id": "prod-1",
+    "title": "Protsessor Intel i5-4590   ",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "I-protsessor_i5_desktop.png"
+    ]
+  },
+  {
+    "id": "prod-2",
+    "title": "Protsessor Intel i5-6400",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "I-protsessor_i5_desktop.png"
+    ]
+  },
+  {
+    "id": "prod-3",
+    "title": "Combo ASUS_P8H61_ i3_3220  Cooler Intel                     ",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "ASUS_P8H61_ i3_3220.webp",
+      "Ema-prose-ASUS_P8H61_ i3_3220_taga.webp"
+    ]
+  },
+  {
+    "id": "prod-4",
+    "title": "Motherboard AM4-880GMA",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "Motherboard_AM4-880GMA_in.webp",
+      "Motherboard_AM4-880GMA.webp"
+    ]
+  },
+  {
+    "id": "prod-5",
+    "title": "Memory Kingston KVR1333 2GB",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "Memory-Kingston_KVR1333_2GB.webp"
+    ]
+  },
+  {
+    "id": "prod-6",
+    "title": "Memory Kingston KVR1333 4GB",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "Memory-Kingston_KVR1333_2GB.webp"
+    ]
+  },
+  {
+    "id": "prod-7",
+    "title": "Monitor 24\" Sam SyncMaster SA450 HD reg. kï¿½rgusega          ",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "Sam-S24E450b.webp",
+      "Sam-S24E450b_label.webp"
+    ]
+  },
+  {
+    "id": "prod-8",
+    "title": "Memory DIMM DDR4 8GB PC4-2133R-UAO-1",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "Memory-DDR4-DIMM.webp"
+    ]
+  },
+  {
+    "id": "prod-9",
+    "title": "Monitor 22ï¿½ ViwSonic",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "ViewSonic_22_eest_odav.webp",
+      "ViewSonic_22_taga_odav.webp"
+    ]
+  },
+  {
+    "id": "prod-10",
+    "title": "Printer HP LJ 110",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "HP-LJ_1010.webp"
+    ]
+  },
+  {
+    "id": "prod-11",
+    "title": "Cartridge Toner HP 4L,4P,4MP                                ",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "Toner-HP-4L_4P_4MP.webp"
+    ]
+  },
+  {
+    "id": "prod-12",
+    "title": "Toner Smasung-ML1710 65g",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "Toner-Smasung-ML1710_65g.webp"
+    ]
+  },
+  {
+    "id": "prod-13",
+    "title": "Cartridge-Samsung-ML1710D3",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "Cartridge-Samsung-ML1710D3.webp"
+    ]
+  },
+  {
+    "id": "prod-14",
+    "title": "Toner-Xerox-3150_3120_3115_3121",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "Toner-Xerox-3150_3120_3115_3121.webp"
+    ]
+  },
+  {
+    "id": "prod-15",
+    "title": "Memory PQI  DDR2-667 1GB",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "DIMM_DDR2_1GB.webp"
+    ]
+  },
+  {
+    "id": "prod-16",
+    "title": "Memory Kingston KTA-MP1066K4_8GB",
+    "categoryL1": "Muu",
+    "categoryL2": "",
+    "price": 0,
+    "images": [
+      "Kingston_KTA-MP1066K4_8GB.webp"
+    ]
+  }
+] as const;
+
+export type Product = typeof allProducts[number];
